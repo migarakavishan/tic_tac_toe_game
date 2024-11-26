129f8cf9c73bf862da654c15a5d7ff05c4ff5660
migarakavishan <migarakavishan43@gmail.com>
2024-11-26 21:01:21 +0530

    "Removed default Flutter demo code and replaced with PlayModeScreen as home screen, added assets directory to pubspec.yaml"

---
+++ lib/screens/play_mode_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class PlayModeScreen extends StatefulWidget {
+  const PlayModeScreen({super.key});
+
+  @override
+  State<PlayModeScreen> createState() => _PlayModeScreenState();
+}
+
+class _PlayModeScreenState extends State<PlayModeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xfff6f6f6),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/cross.png',
+                  width: 100,
+                ),
+                Image.asset(
+                  'assets/circle.png',
+                  width: 100,
+                ),
+
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
