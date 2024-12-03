2f7405f0a2715603219cb99b67c00851b475837e
migarakavishan <migarakavishan43@gmail.com>
2024-12-03 18:58:51 +0530

    "Adjusted image widths from 200 to 100 in PlayModeScreen"

--- lib/screens/play_mode_screen.dart
+++ lib/screens/play_mode_screen.dart
@@ -21,11 +21,11 @@ class _PlayModeScreenState extends State<PlayModeScreen> {
               children: [
                 Image.asset(
                   'assets/cross.png',
-                  width: 200,
+                  width: 100,
                 ),
                 Image.asset(
                   'assets/circle.png',
-                  width: 200,
+                  width: 100,
                 ),
 
               ],
