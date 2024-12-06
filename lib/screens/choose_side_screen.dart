695b868d3210d918da3f34dd42f45acc915d8e4a
migarakavishan <migarakavishan43@gmail.com>
2024-12-06 19:37:40 +0530

    "Updated PlayModeScreen with new UI elements and navigation to ChooseSideScreen"

---
+++ lib/screens/choose_side_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ChooseSideScreen extends StatefulWidget {
+  final bool isAi;
+  const ChooseSideScreen({super.key, required this.isAi});
+
+  @override
+  State<ChooseSideScreen> createState() => _ChooseSideScreenState();
+}
+
+class _ChooseSideScreenState extends State<ChooseSideScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- lib/screens/play_mode_screen.dart
+++ lib/screens/play_mode_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe_game/screens/choose_side_screen.dart';
 
 class PlayModeScreen extends StatefulWidget {
   const PlayModeScreen({super.key});
@@ -21,15 +22,67 @@ class _PlayModeScreenState extends State<PlayModeScreen> {
               children: [
                 Image.asset(
                   'assets/cross.png',
-                  width: 200,
+                  width: 100,
                 ),
                 Image.asset(
                   'assets/circle.png',
                   width: 100,
                 ),
-
               ],
-            )
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Text(
+              'Choose your play mode',
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    minimumSize: const Size(300, 50),
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    elevation: 10,
+                    shadowColor: Colors.blueGrey),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            const ChooseSideScreen(isAi: true),
+                      ));
+                },
+                child: const Text(
+                  "With Ai",
+                  style: TextStyle(fontSize: 24, color: Colors.white),
+                )),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    minimumSize: const Size(300, 50),
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    elevation: 10,
+                    shadowColor: Colors.grey),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            const ChooseSideScreen(isAi: false),
+                      ));
+                },
+                child: const Text(
+                  "With Friend",
+                  style: TextStyle(fontSize: 24, color: Colors.black),
+                )),
           ],
         ),
       ),
